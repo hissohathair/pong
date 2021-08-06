11a24e95a101493b07a7f261ff04ca48ef44654d
Daniel Austin <daniel.austin@gmail.com>
2021-08-07 09:40:00 +1000

    Two different automove implementations
    The second method slightly outperforms the first over long long game plays

--- Player.lua
+++ Player.lua
@@ -20,6 +20,8 @@ function Player:init(playerNumber, paddle, ball_size)
 	self.playerNumber = playerNumber
 	self.paddle = paddle
 	self.ball_size = ball_size
+	self.last_ball_x = 0
+	self.last_ball_y = 0
 end
 
 
@@ -47,11 +49,51 @@ end
 	ball_x, ball_y: Current position of ball
 ]]
 function Player:automove(paddle_speed, ball_x, ball_y)
-    -- simple method: track the ball, try and hit in the middle of the paddle
+	if self.playerNumber == 1 then
+		return self:automove1(paddle_speed, ball_x, ball_y)
+	else
+		return self:automove2(paddle_speed, ball_x, ball_y)
+	end
+	return 0
+end
+
+function Player:automove1(paddle_speed, ball_x, ball_y)
+	-- simple method: track the ball
     if ball_y < self.paddle.y + self.ball_size then
         return -paddle_speed
     elseif ball.y > self.paddle.y + self.paddle.height - self.ball_size then
         return paddle_speed
     end
+    return 0
+end
+
+function Player:automove2(paddle_speed, ball_x, ball_y)
+	-- is the ball moving towards us?
+	movingTowards = true
+	if self.playerNumber == 1 and self.last_ball_x < ball_x then
+		movingTowards = false
+	elseif self.playerNumber == 2 and self.last_ball_x > ball_x then
+		movingTowards = false
+	end
+
+	-- update where the ball was for later comparison
+	self.last_ball_x = ball_x
+	self.last_ball_y = ball_y
+
+    -- simple method: track the ball when it's coming towards us, centre the
+    -- paddle when it's moving away
+    if movingTowards then
+	    if ball_y < self.paddle.y + self.ball_size then
+	        return -paddle_speed
+	    elseif ball.y > self.paddle.y + self.paddle.height - self.ball_size then
+	        return paddle_speed
+	    end
+	else
+		if self.paddle.y < VIRTUAL_HEIGHT / 3 then
+			return paddle_speed
+		elseif self.paddle.y + self.paddle.height > VIRTUAL_HEIGHT * 0.66 then
+			return -paddle_speed
+		end
+	end
     return 0
 end
